4b7b2756cb71202d87cd909c4845a8f9b493787b
Amtresu <conor.follette@gmail.com>
2022-07-23 23:21:16 +0900

    Adding dynamic cards implemented through js

---
+++ script.js
@@ -0,0 +1,77 @@
+const roasterInfo1 = [
+    {
+      name: 'Brandywine',
+      screenshot: 'img/snapshot.svg',
+      location: 'Wilmington, DE',
+      description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
+      
+    },
+    {
+
+      name: 'Birdrock',
+      screenshot: 'img/snapshot.svg',
+      location: 'San-Diego, CA',
+      description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
+    },
+    {
+        name: 'Cat & Cloud',
+        screenshot: 'img/snapshot.svg',
+        location: 'Santa Ana, CA',
+        description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
+      },
+  
+
+  ]
+
+  document.querySelector('.featured-roasters-column-1').innerHTML = roasterInfo1.map((data) =>
+
+  `<div class="featured-roaster">
+          <div class="featured-roaster-img-div">
+            <img class="featured-roaster-img" src="img/featured-roaster-1.webp">
+          </div>
+          <div class="featured-roaster-info">
+            <h4 class="featured-roaster-name">${data.name}</h4>
+            <p class="featured-roaster-location">${data.location}</p>
+            <div class="roaster-seperator"></div>
+            <p>${data.description}</p>
+          </div>
+        </div>`).join('')
+
+
+const roasterInfo2 = [
+      {
+        name: 'Blue Bottle',
+        screenshot: 'img/snapshot.svg',
+        location: 'New York, NY',
+        description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
+      },
+      {
+          name: 'Olympia',
+          screenshot: 'img/snapshot.svg',
+          location: 'Olympia, WA',
+          description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
+      },
+      {
+          name: 'Onyx',
+          screenshot: 'img/snapshot.svg',
+          location: 'Bentonville, AK',
+          description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
+      }
+]
+
+document.querySelector('.featured-roasters-column-2').innerHTML = roasterInfo2.map((data) =>
+
+  `<div class="featured-roaster">
+          <div class="featured-roaster-img-div">
+            <img class="featured-roaster-img" src="img/featured-roaster-1.webp">
+          </div>
+          <div class="featured-roaster-info">
+            <h4 class="featured-roaster-name">${data.name}</h4>
+            <p class="featured-roaster-location">${data.location}</p>
+            <div class="roaster-seperator"></div>
+            <p>${data.description}</p>
+          </div>
+        </div>`).join('')
+
+
+  
