49e2da12ad5ff5fb70ecfb92b785d50c1f485fb2
Amtresu <conor.follette@gmail.com>
2022-07-24 13:32:58 +0900

    adding site js menu and dynamic html cards

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const hamburgerMenu = document.querySelector('.hamburger-icon')
+const popupWindow = document.querySelector('.popup-menu')
+const closePopupWindow = document.querySelector('.close-menu')
+const toggleMenu = false
+
+hamburgerMenu.addEventListener('click', () => {
+  if (toggleMenu === false) {
+    popupWindow.style.visibility = 'visible'
+  }
+})
+
+closePopupWindow.addEventListener('click', () => {
+  if (toggleMenu === false) {
+    popupWindow.style.visibility = 'hidden'
+  }
+})
+
+const navMenuButton = document.querySelector('.popup-menu')
+
+navMenuButton.addEventListener('click', () => {
+  if (toggleMenu === false) {
+    popupWindow.style.visibility = 'hidden'
+  }
+})
+
 const roasterInfo1 = [
     {
       name: 'Brandywine',
@@ -9,16 +34,16 @@ const roasterInfo1 = [
     {
 
       name: 'Birdrock',
-      screenshot: 'img/featured-roaster-1.webp',
+      screenshot: 'img/featured-roaster-6.jpg',
       location: 'San-Diego, CA',
       description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
     },
     {
         name: 'Cat & Cloud',
-        screenshot: 'img/featured-roaster-1.webp',
+        screenshot: 'img/featured-roaster-2.jpg',
         location: 'Santa Ana, CA',
         description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
-      },
+     },
   
 
   ]
@@ -27,7 +52,7 @@ const roasterInfo1 = [
 
   `<div class="featured-roaster">
           <div class="featured-roaster-img-div">
-            <img class="featured-roaster-img" src="${data.screenshot}">
+            <img class="featured-roaster-img" src="${data.screenshot}" alt="Roaster Logo">
           </div>
           <div class="featured-roaster-info">
             <h4 class="featured-roaster-name">${data.name}</h4>
@@ -41,19 +66,19 @@ const roasterInfo1 = [
 const roasterInfo2 = [
       {
         name: 'Blue Bottle',
-        screenshot: 'img/snapshot.svg',
+        screenshot: 'img/featured-roaster-3.jpg',
         location: 'New York, NY',
         description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
       },
       {
           name: 'Olympia',
-          screenshot: 'img/snapshot.svg',
+          screenshot: 'img/featured-roaster-4.png',
           location: 'Olympia, WA',
           description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
       },
       {
           name: 'Onyx',
-          screenshot: 'img/snapshot.svg',
+          screenshot: 'img/featured-roaster-5.jpg',
           location: 'Bentonville, AK',
           description: 'Brandywine is a premier coffee<br> roaster from wilmington, DE'
       }
@@ -63,7 +88,7 @@ document.querySelector('.featured-roasters-column-2').innerHTML = roasterInfo2.m
 
   `<div class="featured-roaster">
           <div class="featured-roaster-img-div">
-            <img class="featured-roaster-img" src="img/featured-roaster-1.webp">
+            <img class="featured-roaster-img" src="${data.screenshot}" alt="Roaster logo">
           </div>
           <div class="featured-roaster-info">
             <h4 class="featured-roaster-name">${data.name}</h4>
